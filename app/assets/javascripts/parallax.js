4af7a4fe0d83baaa4696172626b10989df5da9f9
Jennifer Mah <jen.mah@hotmail.com>
2015-08-03 17:06:12 +0100

    working on function to fix parallax and background-image cover bug

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {  
 
-	$("#first-nav").hover(function() {
-		console.log('hello');
-	  $(this).find("h4").css({"color":"red"});
-	      });
+	// $("#first-nav").hover(function() {
+	// 	console.log('hello');
+	//   $(this).find("h4").css({"color":"red"});
+	//       });
+
+$(window).scroll(function() {
+  var scrolledY = $(window).scrollTop();
+  $('.parallax-one').css('background-position', 'left ' + ((scrolledY)) + 'px');
+});
 
 });
